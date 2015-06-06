096275a54bcf4b730d2445e3995611b024c18e25
Blake Swopes <bswopes@bhodisoft.com>
2015-06-06 16:03:07 -0700

    Not working yet.

--- web/twitter_oauth.php
+++ web/twitter_oauth.php
@@ -26,19 +26,19 @@ if(!empty($_GET['oauth_verifier']) && !empty($_SESSION['oauth_token']) && !empty
 		header('Location: twitter_login.php');
 	} else {
 		// Let's find the user by its ID
-		$query = mysql_query("SELECT * FROM tokens WHERE userid = ". $user_info->id);
-		$result = mysql_fetch_array($query);
-
-        if (!($stmt = $mysqli->prepare("SELECT * FROM tokens WHERE userid = ?"))) {
+        $stmt = $mysqli->stmt_init();
+        if (!($stmt = $mysqli->prepare("SELECT id,userid,oauth_token,oauth_secret FROM tokens WHERE userid = ?"))) {
             echo "Prepare failed: (" . $mysqli->errno . ") " . $mysqli->error;
         }
-        if (!$stmt->bind_param("i", {$user_info->id})) {
+        if (!$stmt->bind_param("i", $user_info->id)) {
             echo "Binding parameters failed: (" . $stmt->errno . ") " . $stmt->error;
         }
         if (!$stmt->execute()) {
             echo "Execute failed: (" . $stmt->errno . ") " . $stmt->error;
         }
-        $result = $stmt->get_result();
+        $result = array();
+        $stmt->bind_result($result['id'],$result['userid'],$result['oauth_token'],$result['oauth_secret']);
+        $stmt->fetch();
 
 		// If not, let's add it to the database
 		if(empty($result)){
@@ -46,14 +46,14 @@ if(!empty($_GET['oauth_verifier']) && !empty($_SESSION['oauth_token']) && !empty
             if (!($stmt = $mysqli->prepare("INSERT INTO tokens (userid, oauth_token, oauth_secret, added) VALUES (?, ?, ?, NOW())"))) {
                 echo "Prepare failed: (" . $mysqli->errno . ") " . $mysqli->error;
             }
-            if (!$stmt->bind_param("iss", {$user_info->id},{$access_token['oauth_token']},{$access_token['oauth_token_secret']})) {
+            if (!$stmt->bind_param("iss", $user_info->id,$access_token['oauth_token'],$access_token['oauth_token_secret'])) {
                echo "Binding parameters failed: (" . $stmt->errno . ") " . $stmt->error;
             }
             if (!$stmt->execute()) {
                 echo "Execute failed: (" . $stmt->errno . ") " . $stmt->error;
             }
 
-            $result = $mysqli->query("SELECT * FROM tokens WHERE id = " . $mysqli->insert_id);
+            $result = $mysqli->query("SELECT id,userid,oauth_token,oauth_secret FROM tokens WHERE id = " . $mysqli->insert_id);
 
 
 		} else {
@@ -61,7 +61,7 @@ if(!empty($_GET['oauth_verifier']) && !empty($_SESSION['oauth_token']) && !empty
             if (!($stmt = $mysqli->prepare("UPDATE tokens SET oauth_token = ?, oauth_secret = ?, accessed = NOW() WHERE userid = ?"))) {
                 echo "Prepare failed: (" . $mysqli->errno . ") " . $mysqli->error;
             }
-            if (!$stmt->bind_param("ssi", {$access_token['oauth_token']},{$access_token['oauth_token_secret']},{$user_info->id})) {
+            if (!$stmt->bind_param("ssi", $access_token['oauth_token'],$access_token['oauth_token_secret'],$user_info->id)) {
                 echo "Binding parameters failed: (" . $stmt->errno . ") " . $stmt->error;
             }
             if (!$stmt->execute()) {
